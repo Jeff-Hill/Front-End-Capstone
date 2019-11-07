65d8989306c388d1f2d94cab509f0ae2a9d623c1
Jeff Hill <jeffhill1978@gmail.com>
2019-11-07 09:50:35 -0600

    profile form update to fix profile form render error

--- src/components/user/ProfileForm.js
+++ src/components/user/ProfileForm.js
@@ -1,4 +1,4 @@
-import {React,  Component } from "react";
+import React,  {Component } from "react";
 import { withRouter } from "react-router-dom";
 import UserManager from "../../modules/UserManager";
 import { Button, Form, FormGroup, Input, FormText } from "reactstrap";
